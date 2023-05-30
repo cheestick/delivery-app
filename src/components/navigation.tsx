1dd5f95c9b98807078d20243f8bab9d7b3559fea
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-30 15:43:37 +0300

    page link active behaviour was added

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,20 +1,10 @@
-import Link from "next/link";
+import PageLink from "./ui/PageLink";
 
 export default function Navigation() {
   return (
     <nav className="flex gap-8">
-      <Link
-        href="/"
-        className="bg-black/5 px-4 py-2 active:bg-black/10 hover:underline underline-offset-8"
-      >
-        Shop
-      </Link>
-      <Link
-        href="cart"
-        className="bg-black/5 px-4 py-2 active:bg-black/10 hover:underline underline-offset-8"
-      >
-        Shopping Cart
-      </Link>
+      <PageLink href="/">Shop</PageLink>
+      <PageLink href="/cart">Shopping Cart</PageLink>
     </nav>
   );
 }
---
+++ src/components/ui/PageLink.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+interface PageLinkProps {
+  children?: ReactNode;
+  href: string;
+}
+
+export default function PageLink({ href, children }: PageLinkProps) {
+  const pathname = usePathname();
+  const isActivePage = href === pathname;
+
+  return (
+    <Link
+      href={href}
+      className={`page-nav ${isActivePage ? "page--current" : ""}`}
+    >
+      {children}
+    </Link>
+  );
+}
