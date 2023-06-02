482157183891b6b0fc539c5db36d65c32a1e005a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-06-02 20:56:28 +0300

    header backdrop blur fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Navigation from "./navigation";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 shadow-xl px-2 py-4">
+    <header className="sticky top-0 shadow-xl px-2 py-4 bg-white/70 backdrop-blur-xl">
       <div className="container mx-auto">
         <Navigation />
       </div>
