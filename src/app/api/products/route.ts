db1436c58386414d9ccd9fa302bb2153dabfb2e8
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-06-03 10:28:09 +0300

    products and shops routes 404 error check was added

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,5 +4,8 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   const products: Product[] = await getAllProducts();
 
+  if (!products)
+    return NextResponse.json({ message: "Not Found" }, { status: 404 });
+
   return NextResponse.json(products);
 }
--- src/app/api/shops/route.ts
+++ src/app/api/shops/route.ts
@@ -4,9 +4,8 @@ import { getAllShops } from "@/services/getAllShops";
 export async function GET(req: Request, res: Response) {
   const shops: Shop[] = await getAllShops();
 
-  if (!shops) {
-    return new Response("Not found", { status: 404 });
-  }
+  if (!shops)
+    return NextResponse.json({ message: "Not Found" }, { status: 404 });
 
   return NextResponse.json(shops);
 }
