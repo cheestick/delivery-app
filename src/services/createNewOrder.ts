03f1e5a235e5f7858d344bfaa770de0a4bf10d4e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-06-05 00:18:04 +0300

    make order function connection to mongodb added

--- src/services/createNewOrder.ts
+++ src/services/createNewOrder.ts
@@ -4,12 +4,17 @@ const URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function createNewOrder(order: Order) {
   const body = JSON.stringify(order);
+  try {
+    const result = await axios.post(`${URL}/orders`, body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  const result = await axios.post(`${URL}/orders`, body, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+    return result;
+  } catch (error) {
+    console.log(error);
 
-  return result;
+    return null;
+  }
 }
--- src/services/makeOrder.ts
+++ src/services/makeOrder.ts
@@ -1,14 +1,19 @@
+import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/Orders";
 
 export async function makeOrder(order: Order) {
-  const newOrder = new Order(order);
-
   try {
+    await mongooseConnect();
+
+    const newOrder = new Order(order);
     await newOrder.validate();
+
     await Order.create(order);
+
     return true;
   } catch (error) {
     console.log(error);
+
     return false;
   }
 }
