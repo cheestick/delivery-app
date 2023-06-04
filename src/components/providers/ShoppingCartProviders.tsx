7d91ac97a0d5f7c4632d7283e17ba4a5f59249cc
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-06-04 14:43:30 +0300

    Context and Providers refactored

--- src/components/providers/ShoppingCartProviders.tsx
+++ src/components/providers/ShoppingCartProviders.tsx
@@ -1,4 +1,8 @@
-import { CredentialsContext, OrderContext } from "@/context/OrderProvider";
+import {
+  CredentialsContext,
+  OrderContext,
+  TotalPriceContext,
+} from "@/context/OrderProvider";
 import { lsCheckoutKey } from "@/utils/constants";
 import { useLocalStorage } from "@react-hooks-library/core";
 import React, { ReactNode, useState } from "react";
@@ -7,14 +11,28 @@ type Props = {
   children: ReactNode;
 };
 
+export const initCredentials: Credentials = {
+  name: "",
+  email: "",
+  phone: "",
+  address: "",
+};
+
 export default function ShoppingCartProviders({ children }: Props) {
-  const [credentials, setCredentials] = useState({});
-  const [checkout, saveCheckout] = useLocalStorage(lsCheckoutKey, []);
+  const [credentials, setCredentials] = useState<Credentials>(initCredentials);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [checkout, saveCheckout] = useLocalStorage<RequiredLSCartItem[]>(
+    lsCheckoutKey,
+    []
+  );
+
   return (
     <OrderContext.Provider value={{ checkout, saveCheckout }}>
-      <CredentialsContext.Provider value={{ credentials, setCredentials }}>
-        {children}
-      </CredentialsContext.Provider>
+      <TotalPriceContext.Provider value={{ totalPrice, setTotalPrice }}>
+        <CredentialsContext.Provider value={{ credentials, setCredentials }}>
+          {children}
+        </CredentialsContext.Provider>
+      </TotalPriceContext.Provider>
     </OrderContext.Provider>
   );
 }
--- src/context/OrderProvider.ts
+++ src/context/OrderProvider.ts
@@ -1,5 +1,34 @@
 import { createContext } from "react";
 
-export const OrderContext = createContext({});
+// Order
+export type OrderDefault = {
+  checkout: RequiredLSCartItem[];
+  saveCheckout: (value: RequiredLSCartItem[]) => void;
+};
 
-export const CredentialsContext = createContext({});
+export const OrderContext = createContext<OrderDefault>({
+  checkout: [],
+  saveCheckout: (f) => {},
+});
+
+// TotalPrice
+export type TotalPriceDefault = {
+  totalPrice: number;
+  setTotalPrice: (value: number) => void;
+};
+
+export const TotalPriceContext = createContext<TotalPriceDefault>({
+  totalPrice: 0,
+  setTotalPrice: (f) => {},
+});
+
+// Credentials
+export type CredentialsDefault = {
+  credentials: Credentials;
+  setCredentials: (value: Credentials) => void;
+};
+
+export const CredentialsContext = createContext<CredentialsDefault>({
+  credentials: { name: "", email: "", phone: "", address: "" },
+  setCredentials: (f) => {},
+});
